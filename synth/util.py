4889a32bde6823d5878938cfff33625f27f6cc23
kbob <kbob@jogger-egg.com>
2019-12-04 18:49:04 -0500

    utility functions convert ratios to/from cents.

--- synth/util.py
+++ synth/util.py
@@ -4,3 +4,11 @@ def MIDI_note_to_freq(note):
     return 440 * 2**((note - 69) / 12)
 
 assert 261 < MIDI_note_to_freq(60) < 262
+
+def cents(n):
+    return 2**(n/1200)
+
+assert cents(1200) == 2
+
+def to_cents(r):
+    return 1200 * log2(r)
